27093caaa5e25539e365ec61e9f815fa43beaac9
aluno <aluno@lab01-13>
2026-03-10 13:39:13 -0300

    sla

---
+++ src/main/java/com/mycompany/programacao01/Programacao01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programacao01;
+
+/**
+ *
+ * @author aluno
+ */
+public class Programacao01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

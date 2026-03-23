96a1c657c9fc42537f8adea0e74dce84d8f62d95
miguel-arc4njo <ardasilvamiguel@gmail.com>
2026-03-23 08:41:55 -0300

    Update professor.java

--- src/main/java/com/mycompany/programacao01/professor.java
+++ src/main/java/com/mycompany/programacao01/professor.java
@@ -25,4 +25,76 @@ public class professor {
         nomeProfessor = "Nobody";
     }
      
+     //nomeProfessor
+      public String getnomeProfessor() {
+        return nomeProfessor;
+    }
+      public void setnomeProfessor(String _nome) {
+        nomeProfessor = _nome;
+    }
+      
+      //SIAPE
+      public String getSIAPE(){
+          return SIAPE;
+      }
+      public void setSIAPE(String _SIAPE){
+          SIAPE = _SIAPE;
+      }
+      
+      //email 
+       public String getEmail() {
+        return email;
+    }  
+    public void getEmail(String _email) {
+        email = _email;
+    }
+    
+    //telefone
+     public String gettelefone() {
+        return telefone;
+    } 
+    public  String setTelefone(String _telefone){
+        return telefone = _telefone;
+    }
+    
+    //titulacaoAcademica
+    public String gettitulacaoAcademica(){
+        return titulacaoAcademica;
+    }
+    public void settitulacaoAcademica(String _titulacaoAcademica){
+        titulacaoAcademica = _titulacaoAcademica;
+    }
+    
+    //areaAtuacao
+    public String getareaAtuacao(){
+        return areaAtuacao;
+    }
+    public void setareaAtuacao(String _areaAtuacao){
+        areaAtuacao = _areaAtuacao;
+    }
+    
+    //regimeTrabalho
+    public String getregimeTrabalho(){
+        return regimeTrabalho;
+    }
+    public void setregimeTrabalho(String _regimeTrabalho){
+        regimeTrabalho = _regimeTrabalho;
+    }
+    
+    //anoIngresso
+    public Date getanoIngresso(){
+        return anoIngresso;
+    }
+    public void setanoIngresso(Date _anoIngresso){
+        anoIngresso = _anoIngresso;
+    }
+    
+    //formacao
+    public String getformacao(){
+        return formacao;
+    }
+    public void setformacao(String _formacao){
+        formacao = _formacao;
+    }
+      
 }

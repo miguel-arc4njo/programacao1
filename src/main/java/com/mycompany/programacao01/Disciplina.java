6c1d3b92d9ce1fa240be55e18f58baff477711eb
miguel-arc4njo <ardasilvamiguel@gmail.com>
2026-03-23 08:41:36 -0300

    Update Disciplina.java

--- src/main/java/com/mycompany/programacao01/Disciplina.java
+++ src/main/java/com/mycompany/programacao01/Disciplina.java
@@ -10,10 +10,48 @@ package com.mycompany.programacao01;
  */
 public class Disciplina {
     private String codigoDisciplina;
-    private String noomeDisciplina;
-    private String cargaHoraria;
+    private String nomeDisciplina;
+    private int cargaHoraria;
     private String ementa;
     private String objetivos;
     
+    //codigoDisciplina
+    public String getcodigoDisciplina(){
+        return codigoDisciplina;
+    }
+    public void setcodigoDisciplina(String _codigoDisciplina){
+        codigoDisciplina = _codigoDisciplina;
+    }
     
+    //noomeDisciplina
+    public String getnomeDisciplina(){
+        return nomeDisciplina;
+    }
+    public void setnoomeDisciplina(String _noomeDisciplina){
+        nomeDisciplina = _noomeDisciplina;
+    }
+    
+    //cargaHoraria
+    public int getcargaHoraria(){
+        return cargaHoraria;
+    }
+    public void setcargaHoraria(int _cargaHoraria){
+        cargaHoraria = _cargaHoraria;
+    }
+    
+    //ementa
+    public String getementa(){
+        return ementa;
+    }
+    public void setementa(String _ementa){
+        ementa = _ementa;
+    }
+    
+    //objetivos
+    public String getobjetivos(){
+        return objetivos;
+    }
+    public void setobjetivos(String _objetivos){
+        objetivos = _objetivos;
+    }
 }
